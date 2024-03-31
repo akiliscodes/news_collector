61b397cb22bc03875b974a0d8d5623f7f4784ae1
akiliscodes <achillesouili@hotmail.fr>
2024-03-31 14:12:54 +0200

    feat: add prefix to data dumps

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -17,7 +17,7 @@ def filter_yaml_files(folder_path):
 
 async def save_to_jsonlines(data, output_folder):
     date_and_time = datetime.datetime.now().strftime("%Y-%m-%d_%H")
-    output_file = os.path.join(output_folder, f"extract_{date_and_time}.jsonl")
+    output_file = os.path.join(output_folder, f"rss_extract_{date_and_time}.jsonl")
     with jsonlines.open(output_file, mode='w') as writer:
         for item in data:
             writer.write(item)
